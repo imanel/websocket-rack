8b58cedc5a9685981ee9e0bb9d30e261404a54e3
Bernard Potocki <bernard.potocki@imanel.org>
2011-02-26 15:48:22 +0100

    Rack is parsing headers for us

--- lib/rack/websocket/handler_factory.rb
+++ lib/rack/websocket/handler_factory.rb
@@ -1,36 +1,13 @@
 module Rack
   module WebSocket
     class HandlerFactory
-      PATH   = /^(\w+) (\/[^\s]*) HTTP\/1\.1$/
-      HEADER = /^([^:]+):\s*(.+)$/
 
       def self.build(connection, data, secure = false, debug = false)
-        (header, remains) = data.split("\r\n\r\n", 2)
-        unless remains
-          # The whole header has not been received yet.
-          return nil
-        end
+        data.rewind
+        remains = data['rack.input'].read
 
-        request = {}
-
-        lines = header.split("\r\n")
-
-        # extract request path
-        first_line = lines.shift.match(PATH)
-        raise HandshakeError, "Invalid HTTP header" unless first_line
-        request['Method'] = first_line[1].strip
-        request['Path'] = first_line[2].strip
-
-        unless request["Method"] == "GET"
+        unless data["REQUEST_METHOD"] == "GET"
           raise HandshakeError, "Must be GET request"
-        end
-
-        # extract query string values
-        request['Query'] = Addressable::URI.parse(request['Path']).query_values ||= {}
-        # extract remaining headers
-        lines.each do |line|
-          h = HEADER.match(line)
-          request[h[1].strip] = h[2].strip if h
         end
 
         version = request['Sec-WebSocket-Key1'] ? 76 : 75
