72f7f1f1e27a4889238f1cb8384020a46892f4ac
Bernard Potocki <bernard.potocki@imanel.org>
2011-02-26 14:32:27 +0100

    Autoload

--- lib/rack/websocket.rb
+++ lib/rack/websocket.rb
@@ -5,7 +5,15 @@ module Rack
 
     autoload :Application,    "#{ROOT_PATH}/websocket/application"
     autoload :Client,         "#{ROOT_PATH}/websocket/client"
+    autoload :Framing03,      "#{ROOT_PATH}/websocket/framing03"
+    autoload :Framing76,      "#{ROOT_PATH}/websocket/framing76"
+    autoload :Handler,        "#{ROOT_PATH}/websocket/handler"
+    autoload :Handler03,      "#{ROOT_PATH}/websocket/handler03"
+    autoload :Handler75,      "#{ROOT_PATH}/websocket/handler75"
+    autoload :Handler76,      "#{ROOT_PATH}/websocket/handler76"
     autoload :HandlerFactory, "#{ROOT_PATH}/websocket/handler_factory"
+    autoload :Handshake75,    "#{ROOT_PATH}/websocket/handshake75"
+    autoload :Handshake76,    "#{ROOT_PATH}/websocket/handshake76"
 
     module Extensions
       autoload :Thin,         "#{ROOT_PATH}/websocket/extensions/thin"
